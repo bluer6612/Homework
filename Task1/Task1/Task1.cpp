8042a88c263621de7f6d27cef5162116d084d665
bluer6612 <gomi6612@gmail.com>
2024-09-23 14:26:02 +0900

    문병무: 과제1 글자수 세기

---
+++ Task1/Task1/Task1.cpp
@@ -0,0 +1,29 @@
+﻿// 000_StringCount.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+// 지금만들라고 하는 함수들은
+// strlen
+
+int StringCount(const char* const _Str)
+{
+    int Count = 0;
+
+    while (_Str[Count])
+    {
+        Count++;
+    }
+
+    return Count;
+}
+
+int main()
+{
+    // [B][B][B][B][0]
+
+    int Count = strlen("BBBB");
+    int Count2 = StringCount("BBBB");
+
+    int a = 0;
+}
