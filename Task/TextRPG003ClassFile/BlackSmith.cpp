f27c096359e7a4fe6723be596c1f55bbee1b35e1
bluer6612 문병무 <gomi6612@gmail.com>
2024-10-07 17:04:23 +0900

    문병무: 과제 9 수정

--- Task/TextRPG003ClassFile/BlackSmith.cpp
+++ Task/TextRPG003ClassFile/BlackSmith.cpp
@@ -78,22 +78,12 @@ void BlackSmith::InPlayer(class UPlayer& _Player/*, int& Result*/)
 					else if (15 >= UpgradeLevel)
 					{
 						UpgradeLevel = 0;
-						printf_s("+d강으로 무기가 파괴 되었습니다.\n", UpgradeLevel);
+						printf_s("+%d강으로 무기가 파괴 되었습니다.\n", UpgradeLevel);
 					}
 				}
-
 				_getch();
 				_Player.SetEquipAtt(UpgradeLevel);
-
 			}
-			// 확률은 전부다 2분의 1
-			// 1돈이 깎여야 한다. 플레이어의 강화 단계 * 100
-			// 내가 현재 5강에 도전하고 있다. 그러면 500원씩 깍여야 한다.
-			// 1~5까지는 실패가 실패확률은 있지만.
-			// 다운되지는 않는다.
-			// 6~10까지는 실패하면 1씩 다운된다.
-			// 10~15는 0으로 실패하면 0으로 떨어진다.
-			// 15강 이상이면 강화를 못하게 해야해한다.
 			break;
 		case '2':
 			return;
